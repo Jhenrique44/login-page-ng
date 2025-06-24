a3626ffa82eabdee40f40a3b5d3de2aa2da8c70d
Jhenrique44 <jo-henrique8@hotmail.com>
2025-06-24 15:44:30 -0300

    feat: creating default login structure

---
+++ src/app/pages/login/login.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { DefaultLayout } from '../../components/default-layout/default-layout';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    DefaultLayout,
+  ],
+  templateUrl: './login.html',
+  styleUrl: './login.scss'
+})
+export class Login {
+
+}
